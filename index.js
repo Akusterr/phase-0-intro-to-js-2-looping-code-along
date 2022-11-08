e64a2c47953025a10f3107ae2557e5c1b6fc00fa
Alexa <kusteralexa@gmail.com>
2022-11-07 22:27:07 -0500

    commit tree 416596b818b271ba07b32d9d7edb5076132758d0

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+
+
+
+function writeCards(arr, event) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    let message = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+    newArray.push(message)
+  }
+  return newArray
+}
+
+
+
